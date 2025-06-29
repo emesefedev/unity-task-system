6bd4cf3e830de2b8c8e66a48dc3a6cec42d6c2e2
emesefedev <emesefedev@gmail.com>
2025-06-29 19:02:40 +0200

    feat: create new TaskSystem in GameManager

---
+++ Assets/Scripts/BattleRoyaleTycoon/CodeMonkey/TaskSystem/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TaskSystem {
+
+    public class GameManager : MonoBehaviour {
+
+        private TaskSystem taskSystem;
+
+        private void Start() {
+            taskSystem = new TaskSystem();
+            
+            Debug.Log(taskSystem.RequestNextTask());
+            
+            TaskSystem.Task task = new TaskSystem.Task();
+            taskSystem.AddTask(task);
+            Debug.Log(taskSystem.RequestNextTask());
+            
+            Debug.Log(taskSystem.RequestNextTask());
+        }
+    }
+}
