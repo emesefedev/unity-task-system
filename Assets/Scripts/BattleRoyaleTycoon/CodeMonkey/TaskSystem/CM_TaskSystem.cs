92fef84f65d1ce70506383148a7037a4a2896028
emesefedev <emesefedev@gmail.com>
2025-06-29 18:49:43 +0200

    import assets

---
+++ Assets/Scripts/BattleRoyaleTycoon/CodeMonkey/TaskSystem/CM_TaskSystem.cs
@@ -0,0 +1,51 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this Code Monkey project
+    I hope you find it useful in your own projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM_TaskSystem {
+
+    public class CM_TaskSystem {
+
+        public class Task {
+            public Vector3 targetPosition;
+        }
+
+        private List<Task> taskList;
+
+        public CM_TaskSystem() {
+            taskList = new List<Task>();
+        }
+
+        public Task RequestNextTask() {
+            // Worker requesting a task
+            if (taskList.Count > 0) {
+                // Give worker the first task
+                Task task = taskList[0];
+                taskList.RemoveAt(0);
+                return task;
+            } else {
+                // No tasks are available
+                return null;
+            }
+        }
+
+        public void AddTask(Task task) {
+            taskList.Add(task);
+        }
+
+
+    }
+
+}
